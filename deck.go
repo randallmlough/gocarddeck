762f571e184ff100ab32126f7e8f0e90020397a7
Randall Lough <randallmlough@gmail.com>
2018-07-30 15:31:58 -0700

    initial commit

---
+++ deck.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+// create a new type of deck
+// which is a slice of strings
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{
+		"Spades",
+		"Clubs",
+		"Diamonds",
+		"Hearts",
+	}
+	cardValues := []string{
+		"Ace",
+		"Two",
+		"Three",
+		"Four",
+		"Five",
+		"Six",
+		"Seven",
+		"Eight",
+		"Nine",
+		"Ten",
+		"Jack",
+		"Queen",
+		"King",
+	}
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i+1, card)
+	}
+}
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+// toString converts the deck type to a single string
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
